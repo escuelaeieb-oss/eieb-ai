70ba42e227aab4cb2a0a89c651313c5b9602915c
escuelaeieb-oss <escuelaeieb@gmail.com>
2026-07-10 19:37:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+from fastapi import FastAPI
 
+app = FastAPI(title="EIEB AI")
+
+@app.get("/")
+def home():
+    return {
+        "mensaje": "Profesor IA de EIEB funcionando"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "ok"
+    }
